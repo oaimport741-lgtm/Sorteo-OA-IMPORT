da8ac5477a38364a3a14ff90c94ffc765cc61d96
oaimport741-lgtm <oaimport741@gmail.com>
2026-03-06 14:43:20 -0300

    script.js

--- script.js
+++ script.js
@@ -4,23 +4,56 @@ const contenedorCodigos = document.getElementById("codigos")
 const loader = document.getElementById("loader")
 
 agregarBtn.addEventListener("click", () => {
+
 const input = document.createElement("input")
+
 input.type = "text"
 input.name = "codigo[]"
 input.placeholder = "Código de Barras del Producto"
 input.required = true
+
 contenedorCodigos.appendChild(input)
+
 })
 
-form.addEventListener("submit", function(e){
+form.addEventListener("submit", async function(e){
+
 e.preventDefault()
+
 loader.style.display = "block"
 
-setTimeout(()=>{
-loader.style.display="none"
-document.getElementById("mensaje").innerText="Datos enviados correctamente"
+const cedula = form.cedula.value
+const nombre = form.nombre.value
+const telefono = form.telefono.value
+const factura = form.factura.value
+
+const codigos = document.querySelectorAll("input[name='codigo[]']")
+
+for (let codigo of codigos){
+
+const data = new FormData()
+
+data.append("cedula", cedula)
+data.append("nombre", nombre)
+data.append("telefono", telefono)
+data.append("factura", factura)
+data.append("codigo", codigo.value)
+
+await fetch("https://script.google.com/macros/s/AKfycbwox13VOVAKANtK1roxIyKtFKFDmP51udX6ZP6Zot_JnKll-p9UIWrwe8elgdDWvw-NWg/exec",{
+method:"POST",
+body:data
+})
+
+}
+
 form.reset()
-},1000)
+
+contenedorCodigos.innerHTML = '<input type="text" name="codigo[]" placeholder="Código de Barras del Producto" required>'
+
+loader.style.display = "none"
+
+document.getElementById("mensaje").innerText = "Datos enviados correctamente"
+
 })
 
 const toggle = document.getElementById("menuToggle")
